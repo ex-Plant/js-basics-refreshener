ddc34177a135c7e49c7df5a90b2b609188169634
ex-Plant <ka@navarra-lab.com>
2024-09-13 10:11:04 +0200

    modules

---
+++ 8_modules/modules.js
@@ -0,0 +1,10 @@
+//DEFAULT VS NORMAL EXPORT
+
+//1. DEFAULT EXPORT
+
+const defaultExport = 'default export';
+
+export default defaultExport
+
+//2. NORMAL EXPORT
+export const normal_export  = `normal export `
