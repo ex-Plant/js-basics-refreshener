7549fe84740c01484133020957e3fdd3333edb06
ex-Plant <ka@navarra-lab.com>
2024-09-18 16:00:10 +0200

    delete node modules

--- src/9_projects/4_countdown/originalCode/code.js
+++ src/9_projects/4_countdown/originalCode/code.js
@@ -1,60 +1,58 @@
-
 let finalDate = null;
 let timerId = null;
 
-document.getElementById("10_countdown-button").addEventListener('click',
-    function(){
-        initTimer();
-    }
-);
-
+document
+  .getElementById("4_countdown-button")
+  .addEventListener("click", function () {
+    initTimer();
+  });
 
 function initTimer() {
-    clearInterval(timerId);
+  clearInterval(timerId);
 
-    finalDate = document.getElementById("final-date-input").value;
-    console.log(finalDate);
+  finalDate = document.getElementById("final-date-input").value;
+  console.log(finalDate);
 
-    if(finalDate == "") return;
-    finalDate = new Date(finalDate);
+  if (finalDate == "") return;
+  finalDate = new Date(finalDate);
 
-    document.getElementById("message").style.display = "none";
-    timer();
-    timerId = setInterval(timer, 1000);
+  document.getElementById("message").style.display = "none";
+  timer();
+  timerId = setInterval(timer, 1000);
 }
 
 function timer() {
-    const now = new Date().getTime();
+  const now = new Date().getTime();
 
-    let interval = (finalDate.getTime() - now) / 1000;
-    interval = Math.floor(interval);
-    console.log(interval);
+  let interval = (finalDate.getTime() - now) / 1000;
+  interval = Math.floor(interval);
+  console.log(interval);
 
-    if(interval <= 0) {
-        document.getElementById("message").style.display = "block";
-        clearInterval(timerId);
-        clearTimer();
-        return;
-    }
+  if (interval <= 0) {
+    document.getElementById("message").style.display = "block";
+    clearInterval(timerId);
+    clearTimer();
+    return;
+  }
 
-    let days = Math.floor(interval / (60*60 * 24));
-    let hours = Math.floor((interval % (60*60 * 24)) / (60 * 60) );
-    let minutes = Math.floor((interval % (60*60)) / 60 );
-    let seconds = Math.floor(interval % 60);
+  let days = Math.floor(interval / (60 * 60 * 24));
+  let hours = Math.floor((interval % (60 * 60 * 24)) / (60 * 60));
+  let minutes = Math.floor((interval % (60 * 60)) / 60);
+  let seconds = Math.floor(interval % 60);
 
-    setHtmlBySelector("#days", days);
-    setHtmlBySelector("#hours", hours);
-    setHtmlBySelector("#minutes", minutes);
-    setHtmlBySelector("#seconds", seconds);
+  setHtmlBySelector("#days", days);
+  setHtmlBySelector("#hours", hours);
+  setHtmlBySelector("#minutes", minutes);
+  setHtmlBySelector("#seconds", seconds);
 }
 
 function setHtmlBySelector(selector, v) {
-    document.querySelector(selector).innerHTML = v;
+  document.querySelector(selector).innerHTML = v;
 }
 
 function clearTimer() {
-    setHtmlBySelector("#days", "-");
-    setHtmlBySelector("#hours", "-");
-    setHtmlBySelector("#minutes", "-");
-    setHtmlBySelector("#seconds", "-");
+  setHtmlBySelector("#days", "-");
+  setHtmlBySelector("#hours", "-");
+  setHtmlBySelector("#minutes", "-");
+  setHtmlBySelector("#seconds", "-");
 }
