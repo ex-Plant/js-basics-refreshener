e85cd0aa116b73d3a1f904fa02662df12aab967d
ex-Plant <ka@navarra-lab.com>
2024-09-18 13:31:37 +0200

    countdown project done

--- src/9_projects/10_countdown/src.js
+++ src/9_projects/10_countdown/src.js
@@ -2,17 +2,80 @@ window.onload = function () {
   timer.init();
 };
 
-function handleCounter(inputDate) {
-  const date = new Date(inputDate);
-  console.log(date);
+function calculateTargetDate(inputDate) {
+  const targetDate = new Date(inputDate).getTime();
+
+  if (!isNaN(targetDate)) {
+    console.error(`🤬🤬🤬 Wrong date format 🤬🤬🤬`);
+    return;
+  }
 
   const now = new Date().getTime();
+  let remainingTimeInSeconds = (targetDate - now) / 1000;
 
-  timer.timerTempDiv.innerHTML = now; // current date
+  const daysLeft = Math.floor(remainingTimeInSeconds / 60 / 60 / 24);
+  remainingTimeInSeconds %= 60 * 60 * 24;
+
+  const hoursLeft = Math.floor(remainingTimeInSeconds / 60 / 60);
+  remainingTimeInSeconds %= 60 * 60;
+
+  const minutesLeft = Math.floor(remainingTimeInSeconds / 60);
+  remainingTimeInSeconds %= 60;
+
+  const secondsLeft = Math.floor(remainingTimeInSeconds);
+  return {
+    secondsLeft,
+    minutesLeft,
+    hoursLeft,
+    daysLeft,
+  };
+}
+
+function populateTimerWithData({
+  secondsLeft,
+  daysLeft,
+  hoursLeft,
+  minutesLeft,
+}) {
+  return `<div>
+            <span>days left: </span>
+            <span>${daysLeft}</span>
+          </div>
+          <div>
+            <span>hours left: </span>
+            <span>${hoursLeft} </span>
+          </div>
+          <div>
+            <span>minutes left: </span>
+            <span>${minutesLeft} </span>
+          </div>
+          <div>
+            <span>seconds left: </span>
+            <span>${secondsLeft} </span>
+          </div>
+`;
+}
+
+function onDateInputChange(inputDate) {
+  if (!inputDate) {
+    console.log(`🤬🤬🤬 You need to select time to continue 🤬🤬🤬 `);
+    return;
+  }
+  if (timer.interval) {
+    clearInterval(timer.interval);
+  }
+
+  timer.interval = setInterval(() => {
+    const targetDate = calculateTargetDate(inputDate);
+    console.log(`executing...`);
+    timer.timerTempDiv.innerHTML = populateTimerWithData(targetDate);
+  }, 1000);
 }
 
 class Timer {
-  constructor() {}
+  constructor() {
+    this.interval = null;
+  }
 
   init = () => {
     console.log(`app is running 🍆🍆🍆`);
@@ -21,10 +84,10 @@ class Timer {
     console.log(this.dateInput.value);
 
     this.dateInput.addEventListener(`change`, () => {
-      handleCounter(this.dateInput.value);
+      onDateInputChange(this.dateInput.value);
     });
 
-    handleCounter(this.dateInput.value);
+    onDateInputChange(this.dateInput.value);
   };
 }
 
