6db8284513dfebea7ca114df543388a6059cc05b
ex-Plant <ka@navarra-lab.com>
2024-09-25 13:00:02 +0200

    salary calc server added

---
+++ src/9_projects/10_salary_calc/run.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+  calculator.init();
+};
+
+class Calculator {
+  construcor() {}
+  init = () => {
+    return console.log("Hello world from the client");
+  };
+}
+
+const calculator = new Calculator();
--- src/9_projects/10_salary_calc/server.js
+++ src/9_projects/10_salary_calc/server.js
@@ -1,29 +1,48 @@
 const http = require(`http`);
 const fs = require("fs").promises;
 const path = require(`path`);
+const cssFile =
+  "/Users/konradantonik/WebstormProjects/nauka_2024/src/output.css";
+
 const server = http.createServer(async (req, res) => {
-  res.setHeader("Content-type", "text/html");
+  console.log(`server initiated 👌`);
 
-  if (req.url === "/") {
-    try {
-      const data = await fs.readFile(path.join(process.cwd(), `index.html`));
-      res.write(data);
+  try {
+    // load javascript
+    if (req.url === "/run.js") {
+      const data = await fs.readFile(path.join(process.cwd(), "run.js"));
       res.status = 200;
+      res.setHeader("Content-type", "application/javascript");
+      res.write(data);
       res.end();
-    } catch (err) {
-      console.log(err);
-      res.status = 500;
-
-      res.end("An error occurred");
-
       return;
     }
-  } else {
-    res.status = 404;
-    res.write(`not found!`);
+
+    if (req.url === "output.css") {
+      // load css
+      const data = await fs.readFile(path.join(cssFile));
+      res.status = 200;
+      res.setHeader("Content-type", "text/css");
+      res.write(data);
+      res.end();
+      return;
+    }
+
+    if (req.url === "/") {
+      res.setHeader("Content-type", "text/html");
+      const data = await fs.readFile(path.join(process.cwd(), `index.html`));
+      res.status = 200;
+      res.write(data);
+      res.end();
+      return;
+    } else {
+      res.status = 404;
+      res.write(`not found!`);
+    }
+  } catch (error) {
+    console.log(`An error occured: `, error);
   }
 });
-
 const port = 8080;
 
 server.listen(port, () => console.log(`Listening on port:`, port));
